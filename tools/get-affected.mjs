e9baeb0c2b0f5766519ca58f806698763abc8c83
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2023-05-30 22:29:27 +0200

    ci

--- tools/get-affected.mjs
+++ tools/get-affected.mjs
@@ -15,12 +15,8 @@ targets.forEach((target) => {
     process.exit(1);
   }
 
-  const value =
-    result.length > 0
-      ? JSON.stringify(result.replace(/\s/g, '').split(','))
-      : '[]';
-  console.log(
-    // e.g. lint=["example-app-seven","example-app-seven-e2e","lib-acorn"] >> $GITHUB_OUTPUT
-    `${target}=${value} >> $GITHUB_OUTPUT`
-  );
+  // e.g. lint=["example-app-seven","example-app-seven-e2e","lib-acorn"] >> $GITHUB_OUTPUT
+  const output = `echo "${target}=[${result}]" >> $GITHUB_OUTPUT`;
+  console.log(output);
+  execSync(output);
 });
